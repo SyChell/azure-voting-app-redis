48212b65ea5fa469aebe992e5079ede80e20aa8d
SyChell <syrinachelly@gmail.com>
2021-06-13 04:16:16 +0200

    added display hostname on top of the app

--- azure-vote/azure-vote/app.py
+++ azure-vote/azure-vote/app.py
@@ -39,8 +39,8 @@ r.ping()
 #    exit('Failed to connect to Redis, terminating.')
 
 # Change title to host name to demo NLB
-if app.config['SHOWHOST'] == "true":
-    title = socket.gethostname()
+# if app.config['SHOWHOST'] == "true":
+title = socket.gethostname()
 
 # Init Redis
 if not r.get(button1): r.set(button1,0)
